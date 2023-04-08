82cf25391bcc43ac04ab026aca6054788c38ff9a
Gabriel Ribeiro <gabrieldeoculos@hotmail.com>
2023-04-08 15:48:13 -0300

    Mudanças.

--- d018.py
+++ d018.py
@@ -1,8 +1,8 @@
 import math
 angulo = float(input("Digite o numero de um angulo qualquer: "))
-coss = math.cos(math.radians(angulo)) #math.cos = Cosseno
-seno = math.sin(math.radians(angulo)) #math.sin = Seno
-tang = math.tan(math.radians(angulo)) #math.tan = Tangente
+coss = math.cos(math.radians(angulo))
+seno = math.sin(math.radians(angulo))
+tang = math.tan(math.radians(angulo))
 print(f"O ângulo de {angulo} possui o SENO de: {seno:.2f}")
 print(f"O ângulo de {angulo} possui o COSSENO de: {coss:.2f}")
 print(f"O ângulo de {angulo} possui a TANGENTE de: {tang:.2f}")
